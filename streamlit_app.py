ae1cab9283323252312ab18ffa82cc274c0b08ea
Korgoz1 <130006816+Korgoz1@users.noreply.github.com>
2023-04-17 14:46:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,8 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+#New Section to display fruityvice api response
+streamlit.header ('Fruityvice Fruit Advice! ')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
