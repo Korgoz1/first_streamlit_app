23f5763e042be90cbed0ea320a4f1d2e9f2a4483
Korgoz1 <130006816+Korgoz1@users.noreply.github.com>
2023-04-17 15:58:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,36 +29,3 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header ('Fruityvice Fruit Advice!')
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-
-fruit_choice:
-        streamlit.error ("Please select a fruit to get information.")
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruitvvice_normalized = pandas.json_normalize(fruitvvice_response.json())
-        streamlit.dataframe(fruitvvice_normalized)
-    
-except URLError as e:
-     streamlit. error ()
-
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# take the ison version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-add_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding a fruit ', add_fruit)
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
