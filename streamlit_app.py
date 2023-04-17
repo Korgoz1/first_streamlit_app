40ab2e6e10b319739cdb2638f9f39d93a3a69e69
Korgoz1 <130006816+Korgoz1@users.noreply.github.com>
2023-04-17 15:37:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,10 +42,10 @@ except URLError as e:
 streamlit.write('The user entered ', fruit_choice)
 
 #import requests
-fruityvice response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the ison version of the response and normalize it
-fruityvice normalized = pandas.json_normalize(fruityvice_response.ison())
+fruityvice normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
